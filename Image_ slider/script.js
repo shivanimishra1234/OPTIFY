c066c1580919a45513adefc8de0474fa56e4d4fc
shivanimishra1234 <156179416+shivanimishra1234@users.noreply.github.com>
2024-08-31 21:35:30 +0530

    Add files via upload

---
+++ Image_ slider/script.js
@@ -0,0 +1,44 @@
+const left=document.querySelector('.left');
+const right=document.querySelector('.right');
+const slider=document.querySelector('.slider');
+const images=document.querySelectorAll('.image')
+let slideNumber=1;
+const length=images.length;
+
+
+
+
+const nextSlide=()=>{
+    slider.style.transform=`translateX(-${slideNumber*800}px)`;
+    slideNumber++;
+
+};
+const prevSlide=()=>{
+    slider.style.transform=`translateX(-${(slideNumber-2)*800}px)`;
+    slideNumber--;};
+const getFirstSlide=()=>{
+    slider.style.transform='translateX(0px)';
+    slideNumber=1;
+};
+const getLastSlide=()=>{
+    slider.style.transform='translateX(-${(length-1)*800px}px)';
+    slideNumber=length;
+};
+right.addEventListener('click',()=>{
+    
+    if(slideNumber<images.length) {
+        nextSlide();
+    }
+    else{
+        getFirstSlide();
+    };
+});
+left.addEventListener('click',()=>{
+    if(slideNumber>1) {
+        prevSlide();
+
+    }
+    else{
+        getLastSlide();
+    };
+});
